e83a08d31e83675a45a07971235a306568ef0fc1
charlesmariga <charlesmariga37@gmail.com>
2022-09-21 17:51:58 +0300

    feat: creations section

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         sans: ["Josefin Sans", "sans-serif"],
         alata: ["Alata"],
       },
+      letterSpacing: {
+        huge: ".3em",
+      },
     },
   },
   plugins: [],
